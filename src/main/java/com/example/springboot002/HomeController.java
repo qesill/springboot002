63caa4557ac421605f402edc6ab054bf8fa35aea
Wiktor Andrzejkiewicz <wiktor.andrzejkiewicz@gmail.com>
2023-07-10 23:20:10 +0200

    home controller

--- src/main/java/com/example/springboot002/HomeController.java
+++ src/main/java/com/example/springboot002/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -18,13 +19,21 @@ public class HomeController {
     public String index(Model model) {
         model.addAttribute("title", title);
         model.addAttribute("users", userRepository.findAll());
-                return "index";
+        return "index";
     }
+
+    @GetMapping("/user/view/{id}")
+    public String singleUserView(@PathVariable("id") int id, Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("user", userRepository.findById(id).get());
+        return "user/view";
+    }
+
     @GetMapping("/home")
     public String home(Model model,
-                        @RequestParam(value = "name",
-                                required = false
-                        ,defaultValue = "Guest") String name) {
+                       @RequestParam(value = "name",
+                               required = false,
+                               defaultValue = "Guest") String name ) {
         model.addAttribute("name", name);
         model.addAttribute("title", title);
         return "home";
